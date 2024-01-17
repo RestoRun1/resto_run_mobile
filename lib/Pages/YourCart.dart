f4d1f76d6ff2d46e9bc582ab66288c178e29a809
Efe Can Tepe <59337173+efecantepe@users.noreply.github.com>
2024-01-18 00:35:56 +0300

    YourCart page works

--- lib/Pages/YourCart.dart
+++ lib/Pages/YourCart.dart
@@ -2,6 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:resto_run_mobile/Color/AppColors.dart';
 import 'package:resto_run_mobile/main.dart';
 import 'package:sizer/sizer.dart';
+import 'package:logger/logger.dart';
+
+List <Map<String, String> >shopItems = [
+
+  {
+    'brand' : "Milk",
+    'description' : "It is a milk",
+    'price' : "165",
+    'image' : "assets/bottle_milk.png"
+  },
+
+  {
+    'brand' : "Milk",
+    'description' : "It is a milk",
+    'price' : "165",
+    'image' : "assets/bottle_milk.png"
+  },
+
+
+];
+
 
 class YourCart extends StatefulWidget {
   const YourCart({super.key});
@@ -18,7 +39,7 @@ class _YourCart extends State<YourCart> {
         child: Container(
           color: AppColors.white,
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0.sp),
+            padding: EdgeInsets.symmetric(horizontal: 8.0.sp, vertical: 16.0.sp),
             child:
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               SizedBox(
@@ -34,62 +55,123 @@ class _YourCart extends State<YourCart> {
               SizedBox(
                 height: 1.5.h,
               ),
-              getProduct(context)
+
+              Flexible(
+                child: ListView(
+                  shrinkWrap: false,
+                  physics: ClampingScrollPhysics(),
+                  children: shopItems.map((e) => Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: (ShoppingCartItem(shopItem: e)),
+                  )).toList(),
+                ),
+              ),
+
+              SizedBox(height: 3.h,),
+
+              SizedBox(height: 3.h,),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Total", style: TextStyle(color: AppColors.textGrey, fontSize: 18 , fontWeight: FontWeight.bold ),),
+                  Text("₹ 1,527", style: TextStyle(color: AppColors.lightGreen, fontSize: 20, fontWeight: FontWeight.bold),)
+                ],
+              ),
+              
+              SizedBox(height: 3.h,),
+
+              AspectRatio(
+                aspectRatio: 335 / 62,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.lightGreen
+                  ),
+                  onPressed: () => print("Proceed To Checkout is added"),
+                  child: Text("Proceed to checkout", style: TextStyle(color: AppColors.white),),
+                  ),
+              )
+
+              //getProduct(context)
             ]),
           ),
         ),
       ),
     );
   }
+}
 
-  Widget getProduct(BuildContext context) {
-    return Sizer(
-        builder: (context, orientation, deviceType) => Container(
-              
-              color: AppColors.productBackColor ,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                        flex: 1,
-                        child: Container(
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(234, 221, 205,1),
-                            borderRadius: BorderRadius.circular(16)
-                          ),
-                          child: Image.asset("assets/bottle_milk.png", fit: BoxFit.contain,)
-                        )  
-                      ),
+class ShoppingCartItem extends StatefulWidget {
 
-                    SizedBox(width: 2.w,),
+  final Map<String, String> shopItem;
 
-                    Expanded(
-                        flex: 2,
-                        child: Container(
-                          alignment: Alignment.centerLeft,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Lauren's", style: TextStyle(color: AppColors.productText, fontSize: 10.sp, fontWeight: FontWeight.bold),),
-                              Text("Orange Juice", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12.sp, color: AppColors.productText),),
-                              Text("TL 149", style: TextStyle( fontWeight: FontWeight.bold,fontSize: 12.sp ,color: AppColors.lightGreen),),
-                            ]),
-                        )  
-                      ),
-                    Expanded(
-                        flex: 1,
-                         child: Container(
-                          color: Colors.red,
-                           child: Text(
-                            "Efe"
-                          ),
-                         )     
-                    ),
-                  ],
-                ),
+  const ShoppingCartItem({super.key,required this.shopItem});
+
+  @override
+  State<ShoppingCartItem> createState() => _ShoppingCartItem();
+}
+
+class _ShoppingCartItem extends State<ShoppingCartItem> {
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      decoration: BoxDecoration(
+        color: AppColors.productBackColor,
+        borderRadius: BorderRadius.circular(16)    
+      ),
+      
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(234, 221, 205,1),
+                  borderRadius: BorderRadius.circular(16)
               ),
-            ));
+              child: Image.asset(widget.shopItem["image"]!, height: 100,)
+            ),
+            
+            SizedBox(width: 3.w,),
+
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+              
+                children: [
+                  Text(widget.shopItem["brand"]!, style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold),),
+                  Text(widget.shopItem["description"]!, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold) ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(widget.shopItem['price']!, style: TextStyle(color: AppColors.lightGreen, fontSize: 18, fontWeight: FontWeight.bold),),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: AppColors.white
+                        ),
+                        child: Row(
+                          children: [ 
+                            IconButton(onPressed:() => print("Minus Is Clicked"), icon: Icon(Icons.remove, color: AppColors.lightGreen,)),
+                                
+                            Text("1", ),
+                                
+                            IconButton(onPressed:() => print("Plus Is Clicked"), icon: Icon(Icons.add, color: AppColors.lightGreen)),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
